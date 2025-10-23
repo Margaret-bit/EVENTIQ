4af67308c20bf54fe7d0def9b21ef36476e4ee94
Eseh Emmanuel <your@email.com>
2025-10-23 01:56:12 -0700

    Did a routing page

---
+++ src/Components/Outdoor.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Outdoor = () => {
+  return <Out>Outdoor</Out>;
+};
+
+export default Outdoor;
+const Out = styled.div`
+  width: 100%;
+  height: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: green;
+`;
