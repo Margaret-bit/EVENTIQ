9b04eb318c0bb7d1148b441a149c9f3b85408ecd
unknown <successdubizu.com>
2025-10-24 14:08:47 +0100

    i worked on eventiqdashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import entiqDashboard from "./Components/EventiqDashboard";
+import MyVenues from "./Components/MyVenues";
 const App = () => {
-  return <div></div>;
+  Ev;
+  return (
+    <div>
+      <EventiqDashboard />
+      <MyVenues />
+    </div>
+  );
 };
 
 export default App;
